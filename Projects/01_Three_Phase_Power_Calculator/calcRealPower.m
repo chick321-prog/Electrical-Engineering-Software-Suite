ef1a60524f00cb5e5d7306c39ecda879f423aab5
Thota Sadhika <chickshop321@gmail.com>
2026-07-26 18:38:51 +0530

    Add files via upload

--- Projects/01_Three_Phase_Power_Calculator/calcRealPower.m
+++ Projects/01_Three_Phase_Power_Calculator/calcRealPower.m
@@ -1,3 +1,24 @@
+%==================================================
+% File Name   : calcRealPower.m
+% Project     : Three-Phase Power Calculator
+% Description : Calculates the real power in a
+%               three-phase AC system.
+%
+% Formula:
+% P = √3 × V × I × pf
+%
+% Inputs:
+% voltage - Line Voltage (V)
+% current - Line Current (A)
+% pf      - Power Factor
+%
+% Output:
+% realPower - Real Power (W)
+%
+% Author : Thota Sadhika
+% Version: 1.0
+%==================================================
+
 function realPower = calcRealPower(voltage, current, pf)
 
     realPower = sqrt(3) * voltage * current * pf;
