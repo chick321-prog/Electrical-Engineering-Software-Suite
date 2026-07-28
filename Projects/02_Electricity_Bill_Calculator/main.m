75a98f2ad076d514348d6ee6706c76b02cbc054d
Thota Sadhika <chickshop321@gmail.com>
2026-07-28 07:10:54 +0530

    Update main.m

--- Projects/02_Electricity_Bill_Calculator/main.m
+++ Projects/02_Electricity_Bill_Calculator/main.m
@@ -1,1 +1,37 @@
+%==================================================
+% File Name   : main.m
+% Project     : Electricity Bill Calculator
+% Description : Main program for calculating
+%               Electricity Bill.
+%
+% Author      : Thota Sadhika
+% Version     : 1.1
+%==================================================
 
+clc;
+clear;
+
+fprintf("=========================================\n");
+fprintf("     Electricity Bill Calculator\n");
+fprintf("             Version 1.1\n");
+fprintf("=========================================\n\n");
+
+% User Inputs
+power = input("Enter Power (W): ");
+hours = input("Enter Hours (hr): ");
+days = input("Enter Days : ");
+
+fprintf("\nPerforming Calculations...\n\n");
+
+% Energy
+energy = calculateEnergy(power, hours, days);
+fprintf("Energy      : %.2f kWh\n", energy);
+
+% Bill
+bill = calculateBill(energy);
+fprintf("Bill        : Rs. %.2f\n", bill);
+
+
+fprintf("\n=========================================\n");
+fprintf("Calculation Completed Successfully.\n");
+fprintf("=========================================\n");
