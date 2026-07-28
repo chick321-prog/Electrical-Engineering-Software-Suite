8e18e9fc92e27375144d4b5a6062b158dbdadb76
Thota Sadhika <chickshop321@gmail.com>
2026-07-28 07:09:10 +0530

    Update calculateBill.m

--- Projects/02_Electricity_Bill_Calculator/calculateBill.m
+++ Projects/02_Electricity_Bill_Calculator/calculateBill.m
@@ -1,1 +1,37 @@
+%===================================================
+%File name    : calculateBill.m
+%project      : Electricity Bill Calculator
+% Description : Calculates Bill
+%
+% Formula:
+% Bill = Energy * Rate
+%
+% Inputs:
+% energy = Energy Consumption(kWh)
+%
+% Output:
+% bill - Bill Calculation
+%
+% Author : Thota Sadhika
+% Version: 1.0
+%==================================================
 
+function bill = calculateBill(energy)
+    if energy <= 100
+
+      bill = energy * 1.5;
+
+    elseif energy <= 200
+
+      bill = energy * 2.5;
+
+    elseif energy <= 300
+
+      bill = energy * 4.0;
+
+    else
+
+      bill = energy * 6.0;
+
+    end
+end
