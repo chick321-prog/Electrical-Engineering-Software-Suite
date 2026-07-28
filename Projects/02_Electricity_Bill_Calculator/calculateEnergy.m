44a785d3e03f75bc25d3e260c47586b5b5889ab9
Thota Sadhika <chickshop321@gmail.com>
2026-07-28 07:10:24 +0530

    Update calculateEnergy.m

--- Projects/02_Electricity_Bill_Calculator/calculateEnergy.m
+++ Projects/02_Electricity_Bill_Calculator/calculateEnergy.m
@@ -1,1 +1,26 @@
+%=========================================
+%File name    : calculateEnergy.m
+%Project      : Electricity Bill Calculator
+% Description : Calculates electrical energy
+%               consumption in kilowatt-hours (kWh).
+%
+% Formula:
+% Energy   =    (Power × Hours × Days) / 1000
+%
+% Inputs:
+% power    -    Power Rating (W)
+% hours    -    Operating Hours per Day
+% days     -    Number of Days
+%
+% Output:
+% energy   -    Energy Consumption (kWh)
+%
+% Author : Thota Sadhika
+% Version: 1.0
+%==================================================
 
+function energy = calculateEnergy(power, hours, days)
+
+    energy = (power * hours * days) / 1000;
+
+end
